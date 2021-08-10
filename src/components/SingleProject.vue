45977131de71516ff38f977148a36c44e840787c
Kyaw Lwin Moe <kyawlwinmoe.taungyi.myanmar@gmail.com>
2021-08-10 11:25:50 +0630

    Second Commit

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,8 +1,26 @@
 <template>
 <div class="project">
-    <h3 @click="showDetail=!showDetail">{{project.title}}</h3>
-  <p v-if="showDetail">{{project.detail}}</p>
+    <div class="flexBox">
+        <div>
+            <h3 @click="showDetail=!showDetail">{{project.title}}</h3>
+            <p v-if="showDetail">{{project.detail}}</p>
+
+        </div>
+        <div>
+            <span class="material-icons">
+                delete
+            </span>
+            <span class="material-icons">
+                edit
+            </span>
+            <span class="material-icons">
+                done
+            </span>
+
+        </div>
+    </div>
 </div>
+
 </template>
 
 <script>
@@ -22,10 +40,26 @@ export default {
     padding: 20px;
     background-color: #f2f2f2;
     margin: 10px auto;
-    cursor: pointer;
+    border-left: 10px solid crimson;
+    border-radius: 10px ;
 }
 h3{
     color: indigo;
+    cursor: pointer;
+
 }
+.flexBox{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+span{
+    margin-left: 10px;
+    cursor: pointer;
+
+}
+    span:hover{
+        color: blue;
+    }
 
 </style>
