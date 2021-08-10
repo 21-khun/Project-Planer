479c4dd7b2e6c41c8635f2967ecd7cea054b8d00
Kyaw Lwin Moe <kyawlwinmoe.taungyi.myanmar@gmail.com>
2021-08-10 10:52:44 +0630

    First Commit

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="project">
+    <h3 @click="showDetail=!showDetail">{{project.title}}</h3>
+  <p v-if="showDetail">{{project.detail}}</p>
+</div>
+</template>
+
+<script>
+export default {
+    props:['project'],
+    data(){
+        return{
+            showDetail:false
+        }
+    }
+
+}
+</script>
+
+<style>
+.project{
+    padding: 20px;
+    background-color: #f2f2f2;
+    margin: 10px auto;
+    cursor: pointer;
+}
+h3{
+    color: indigo;
+}
+
+</style>
